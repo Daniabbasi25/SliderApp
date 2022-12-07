18026d19972c1ce5fecf9651a4871bbbc8e32d71
Danyal Muneer <abbasidani82@gmail.com>
2022-12-07 14:55:48 +0500

    uploading project

---
+++ app/services/firebaseConfig.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  enabled: false,
+  config: {
+    apiKey: "AIzaSyDXdO1QIsPLYEXuoQzZ74WCSmX5bnBIn-E",
+    authDomain: "maudeal-d213e.firebaseapp.com",
+    projectId: "maudeal-d213e",
+    storageBucket: "maudeal-d213e.appspot.com",
+    messagingSenderId: "422661746750",
+    appId: "1:422661746750:web:76339c56f5e17c61eda16f",
+    measurementId: "G-V21Y9DDNKD",
+  },
+};
